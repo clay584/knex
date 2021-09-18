7fec1cb1368928add3e87c8b9ff2605719c60681
Clay Curtis <clay584@gmail.com>
2021-09-18 11:35:30 -0400

    lots of tests...92% coverage

--- tests/test_knex.py
+++ tests/test_knex.py
@@ -83,60 +83,6 @@ def test_index_no_raise():
         Start(Parser, raise_exception=False) > GetIndex(1)
     ).result == "'type' object is not subscriptable"
 
-
-def test_get_field():
-    assert (  # nosec B101
-        Start({"foo": "bar", "buzz": "baz"}) > GetField("foo")
-    ).result == "bar"
-
-
-def test_count():
-    assert (Start({"foo": "bar", "buzz": "baz"}) > Count()).result == 2  # nosec B101
-
-
-def test_to_lower():
-    assert (Start("FOOBAR") > ToLower()).result == "foobar"  # nosec B101
-
-
-def test_to_upper():
-    assert (Start("foobar") > ToUpper()).result == "FOOBAR"  # nosec B101
-
-
-def test_ip_network():
-    assert (  # nosec B101
-        Start("192.168.1.55/24") > IpNetwork()
-    ).result == "192.168.1.0/24"
-
-
-def test_regex_extract():
-    rtr_output = """
-Interface             IP-Address      OK?    Method Status     	Protocol
-GigabitEthernet0/1    unassigned      YES    unset  up         	up
-GigabitEthernet0/2    192.168.190.235 YES    unset  up         	up
-GigabitEthernet0/3    unassigned      YES    unset  up         	up
-GigabitEthernet0/4    192.168.191.2   YES    unset  up         	up
-TenGigabitEthernet2/1 unassigned      YES    unset  up         	up
-TenGigabitEthernet2/2 unassigned      YES    unset  up         	up
-TenGigabitEthernet2/3 unassigned      YES    unset  up         	up
-TenGigabitEthernet2/4 unassigned      YES    unset  down       	down
-GigabitEthernet36/1   unassigned      YES    unset  down        down
-GigabitEthernet36/2   unassigned      YES    unset  down        down
-GigabitEthernet36/11  unassigned      YES    unset  down       	down
-GigabitEthernet36/25  unassigned      YES    unset  down       	down
-Te36/45               unassigned      YES    unset  down       	down
-Te36/46               unassigned      YES    unset  down       	down
-Te36/47               unassigned      YES    unset  down       	down
-Te36/48               unassigned      YES    unset  down       	down
-Virtual36             unassigned      YES    unset  up         	up
-"""
-
-    pattern = r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b"
-    assert (Start(rtr_output) > RegexExtractAll(pattern)).result == [  # nosec B101
-        "192.168.190.235",
-        "192.168.191.2",
-    ]
-
-
 def test_chain1():
     assert (  # nosec B101
         Start("clay,michelle")
@@ -204,3 +150,203 @@ def test_base64decode():
     assert (  # nosec B101
         Start(["Clay"]) > Base64Decode()
     ).result == "argument should be a bytes-like object or ASCII string, not 'list'"
+
+
+def test_to_upper_success():
+    assert (Start("foobar") > ToUpper()).result == "FOOBAR"  # nosec B101
+
+
+def test_to_upper_raise():
+    try:
+        (Start(["foobar"], raise_exception=True) > ToUpper())
+    except Exception as e:
+        assert type(e).__name__ == "AttributeError"  # nosec B101
+
+
+def test_to_upper_fail():
+    assert (
+        Start(["foobar"]) > ToUpper()
+    ).result == "'list' object has no attribute 'upper'"  # nosec B101
+
+
+def test_get_field_success():
+    assert (Start({"foo": "bar"}) > GetField("foo")).result == "bar"  # nosec B101
+
+
+def test_get_field_raise():
+    try:
+        (Start(["foobar"], raise_exception=True) > GetField("foo"))
+    except Exception as e:
+        assert type(e).__name__ == "TypeError"  # nosec B101
+
+
+def test_get_field_fail():
+    assert (  # nosec B101
+        Start(["foobar"]) > GetField("foo")
+    ).result == "list indices must be integers or slices, not str"
+
+
+
+
+def test_count_success():
+    assert (Start({"foo": "bar"}) > Count()).result == 1  # nosec B101
+
+
+def test_count_raise():
+    try:
+        (Start("", raise_exception=True) > Count())
+    except Exception as e:
+        assert type(e).__name__ == "TypeError"  # nosec B101
+
+
+def test_count_fail():
+    assert (  # nosec B101
+        Start(Parser) > Count()).result == "object of type 'type' has no len()"
+
+
+def test_to_lower_success():
+    assert (Start("FOOBAR") > ToLower()).result == "foobar"  # nosec B101
+
+def test_to_lower_raise():
+    try:
+        (Start([], raise_exception=True) > ToLower())
+    except Exception as e:
+        assert type(e).__name__ == "AttributeError"  # nosec B101
+
+
+def test_to_lower_fail():
+    assert (  # nosec B101
+        Start(Parser) > ToLower()).result == "type object 'Parser' has no attribute 'lower'"
+
+def test_ip_network_success():
+    assert (  # nosec B101
+        Start("192.168.1.55/24") > IpNetwork()
+    ).result == "192.168.1.0/24"
+
+def test_ip_network_raise():
+    try:
+        (Start("", raise_exception=True) > IpNetwork())
+    except Exception as e:
+        assert type(e).__name__ == "AddressValueError"  # nosec B101
+
+
+def test_ip_network_fail():
+    assert (  # nosec B101
+        Start("") > IpNetwork()).result == "Address cannot be empty"
+
+def test_regex_extract_success():
+    rtr_output = """
+Interface             IP-Address      OK?    Method Status     	Protocol
+GigabitEthernet0/1    unassigned      YES    unset  up         	up
+GigabitEthernet0/2    192.168.190.235 YES    unset  up         	up
+GigabitEthernet0/3    unassigned      YES    unset  up         	up
+GigabitEthernet0/4    192.168.191.2   YES    unset  up         	up
+TenGigabitEthernet2/1 unassigned      YES    unset  up         	up
+TenGigabitEthernet2/2 unassigned      YES    unset  up         	up
+TenGigabitEthernet2/3 unassigned      YES    unset  up         	up
+TenGigabitEthernet2/4 unassigned      YES    unset  down       	down
+GigabitEthernet36/1   unassigned      YES    unset  down        down
+GigabitEthernet36/2   unassigned      YES    unset  down        down
+GigabitEthernet36/11  unassigned      YES    unset  down       	down
+GigabitEthernet36/25  unassigned      YES    unset  down       	down
+Te36/45               unassigned      YES    unset  down       	down
+Te36/46               unassigned      YES    unset  down       	down
+Te36/47               unassigned      YES    unset  down       	down
+Te36/48               unassigned      YES    unset  down       	down
+Virtual36             unassigned      YES    unset  up         	up
+"""
+
+    pattern = r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b"
+    assert (Start(rtr_output) > RegexExtractAll(pattern)).result == [  # nosec B101
+        "192.168.190.235",
+        "192.168.191.2",
+    ]
+
+def test_regex_extract_success2():
+    input_str = "I've got a lovely bunch of coconuts."
+    pattern = r"coco(\S+)\."
+    assert (Start(input_str) > RegexExtractAll(pattern)).result[0] == "nuts"  # nosec B101
+
+def test_regex_extract_raise():
+    try:
+        (Start([], raise_exception=True) > RegexExtractAll("asdf"))
+    except Exception as e:
+        assert type(e).__name__ == "TypeError"  # nosec B101
+
+
+def test_regex_extract_fail():
+    assert (  # nosec B101
+        Start([]) > RegexExtractAll("asdf")).result == "expected string or buffer"
+
+
+
+
+
+
+
+
+
+
+# def test__success():
+#     assert (Start() > ).result ==    # nosec B101
+
+# def test__fail():
+#     assert (Start() > ).result ==   # nosec B101
+
+# def test__raise():
+#     try:
+#         (Start() > Foo(raise_exception=True))
+#     except Exception as e:
+#         assert type(e).__name__ == ""  # nosec B101
+
+
+
+
+
+
+# def test__success():
+#     assert (Start() > ).result ==    # nosec B101
+
+# def test__fail():
+#     assert (Start() > ).result ==   # nosec B101
+
+# def test__raise():
+#     try:
+#         (Start() > Foo(raise_exception=True))
+#     except Exception as e:
+#         assert type(e).__name__ == ""  # nosec B101
+
+
+
+
+
+# def test__success():
+#     assert (Start() > ).result ==    # nosec B101
+
+# def test__fail():
+#     assert (Start() > ).result ==   # nosec B101
+
+# def test__raise():
+#     try:
+#         (Start() > Foo(raise_exception=True))
+#     except Exception as e:
+#         assert type(e).__name__ == ""  # nosec B101
+
+
+
+
+
+
+
+# def test__success():
+#     assert (Start() > ).result ==    # nosec B101
+
+# def test__fail():
+#     assert (Start() > ).result ==   # nosec B101
+
+# def test__raise():
+#     try:
+#         (Start() > Foo(raise_exception=True))
+#     except Exception as e:
+#         assert type(e).__name__ == ""  # nosec B101
+
