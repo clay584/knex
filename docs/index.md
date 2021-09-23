d89b2f995e8717bb10dd2f20b0d7041dc2901223
Clay Curtis <clay584@gmail.com>
2021-09-23 18:23:31 -0400

    Bump version: 0.2.8 → 0.2.9

--- docs/index.md
+++ docs/index.md
@@ -1,4 +1,4 @@
-# Welcome to Knex (v0.2.8)
+# Welcome to Knex (v0.2.9)
 
 Knex is a Python library for creating chainable data transformers.
 
--- knex/__init__.py
+++ knex/__init__.py
@@ -1,5 +1,5 @@
 """knex: Python library for creating chainable data transformers."""
-__version__ = "0.2.8"
+__version__ = "0.2.9"
 
 __all__ = [
     "Start",
