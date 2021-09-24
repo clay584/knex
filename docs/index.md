a7a54bb8c010a718d9f847df009c9ae0f6649961
Clay Curtis <clay584@gmail.com>
2021-09-24 13:42:50 -0400

    Bump version: 0.2.16 → 0.2.17

--- docs/index.md
+++ docs/index.md
@@ -1,4 +1,4 @@
-# Welcome to Knex (v0.2.16)
+# Welcome to Knex (v0.2.17)
 
 Knex is a Python library for creating chainable data transformers.
 
--- tests/test_knex.py
+++ tests/test_knex.py
@@ -23,7 +23,7 @@ from knex.parsers import (
 
 
 def test_version():
-    assert __version__ == "0.2.16"  # nosec B101
+    assert __version__ == "0.2.17"  # nosec B101
 
 
 def test_parser():
